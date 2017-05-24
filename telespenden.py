bc613ccf21768e42b13f60eaa27f26d1d2b469f8
Noah Biederbeck <n.biederbeck@gmx.de>
2017-05-24 06:43:29 +0000

    telespenden bug fixed

--- telespenden.py
+++ telespenden.py
@@ -55,9 +55,9 @@ def checktime():
     seconds = time.localtime()[5]
     timeInSeconds = hours * 60 * 60 + minutes * 60 + seconds
     sleeptime = 1 + 10 * 60 + 24 * 60 * 60 - timeInSeconds
-    ''' TEXT = "Ich warte jetzt %i:%i h." % (sleeptime//3600, (sleeptime % 3600)//60) '''
+    TEXT = "Ich warte jetzt %i:%i h." % (sleeptime//3600, (sleeptime % 3600)//60)
     print(TEXT)
-    BOT.sendMessage(chat_id=CHAT_ID, text=TEXT)
+    # BOT.sendMessage(chat_id=CHAT_ID, text=TEXT)
     time.sleep(sleeptime)
 
 
