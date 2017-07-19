01247c2b5d5b79e5792eef7773649a6637073f16
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-07-19 20:50:31 +0200

    Update start_bots.sh

--- start_bots.sh
+++ start_bots.sh
@@ -2,3 +2,5 @@
 # Used to clear PIDs and start every bot.
 echo "" > process_ids.txt
 $HOME/.conda/envs/bot/bin/python bot.py
+$HOME/.conda/envs/bot/bin/python traffic.py
+$HOME/.conda/envs/bot/bin/python raspberrysudo.py
