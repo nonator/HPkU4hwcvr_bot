0421b9fb94eca52deb7ea512b5cd3e8bdf8018fd
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-07-19 20:48:56 +0200

    Update kill_bots.sh

--- kill_bots.sh
+++ kill_bots.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 # Used to kill all bots given the process id in the file process_ids.txt,
 # which the bots write their PID into.
-kill `cat $HOME/Git/nonator/raspberryPiBot/process_ids.txt`
+kill `cat process_ids.txt`
+echo "" > process_ids.txt
