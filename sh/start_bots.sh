b78c19a065734b8f12d9cc78bdbaa7f7db74ab92
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-07-20 15:05:51 +0200

    work now

--- sh/start_bots.sh
+++ sh/start_bots.sh
@@ -3,5 +3,3 @@
 echo "" > /home/pi/Git/RaspberryPiBot/process_ids.txt
 nohup /home/pi/miniconda3/envs/bot/bin/python /home/pi/Git/cryptobot/bot.py > /dev/null 2>&1 &
 nohup /home/pi/miniconda3/envs/bot/bin/python /home/pi/Git/RaspberryPiBot/bots/traffic.py > /dev/null 2>&1 &
-# /home/pi/miniconda3/envs/bot/bin/python /home/pi/Git/cryptobot/bot.py
-# /home/pi/miniconda3/envs/bot/bin/python /home/pi/Git/RaspberryPiBot/bots/traffic.py
