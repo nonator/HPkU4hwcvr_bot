8c723cf903fdd70e446a4c300a66be8dea3a6908
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-08-06 14:47:51 +0200

    logger working

--- bots/logger.py
+++ bots/logger.py
@@ -1,3 +1,7 @@
-import bot
+from telepot import Bot
+TOKEN = '342565093:AAEd2KenIzjmzxZxmSek3fxq-eQRrAWIp5Q'
+CHAT_ID = 322086570
+BOT = Bot(TOKEN)
 
-bot.send('/home/pi/bot.log')
+with open('/home/pi/bot.log', 'r') as f:
+    BOT.sendDocument(chat_id=CHAT_ID, document=f, caption='bot.log')
