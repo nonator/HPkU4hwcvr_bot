9e3847f3a9917355564416a2f8a7acd718e698c0
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-08-06 13:21:18 +0200

    log into file

--- sh/git_pull.sh
+++ sh/git_pull.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 # git pull in every directory
 cd /home/pi/Git/RaspberryPiBot
-git stash > /dev/null 2>&1
-git pull > /dev/null 2>&1
+git stash > /home/π/bot.log 2>&1
+git pull > /home/π/bot.log 2>&1
+# git stash > /dev/null 2>&1
+# git pull > /dev/null 2>&1
 cd /home/pi/Git/cryptobot
-git stash > /dev/null 2>&1
-git pull > /dev/null 2>&1
+git stash > /home/π/bot.log 2>&1
+git pull > /home/π/bot.log 2>&1
+# git stash > /dev/null 2>&1
+# git pull > /dev/null 2>&1
 cd $HOME
