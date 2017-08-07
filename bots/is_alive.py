5fd24d38bad9ecc1afb97ceb741044866ea169c3
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-08-07 16:09:09 +0200

    remove test message

--- bots/is_alive.py
+++ bots/is_alive.py
@@ -8,4 +8,4 @@ BOT = Bot(TOKEN)
 BOT.sendMessage(chat_id=CHAT_ID,
         parse_mode='Markdown',
         disable_notification=True,
-        text='`I am still alive.\nAnd this is a second Test.`')
+        text='`I am still alive.`')
