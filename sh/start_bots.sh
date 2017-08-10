d1925df826a4f151b7a55618b3ffa72f201f66c4
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-08-10 08:35:36 +0200

    insert missing &

--- sh/start_bots.sh
+++ sh/start_bots.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Used to clear PIDs and start every bot.
-nohup /home/pi/miniconda3/envs/bot/bin/python /home/pi/Git/cryptobot/bot.py >> /home/pi/bot.log 2>&1
-nohup /home/pi/miniconda3/envs/bot/bin/python /home/pi/Git/RaspberryPiBot/bots/traffic.py >> /home/pi/bot.log 2>&1
+nohup /home/pi/miniconda3/envs/bot/bin/python /home/pi/Git/cryptobot/bot.py >> /home/pi/bot.log 2>&1 &
+nohup /home/pi/miniconda3/envs/bot/bin/python /home/pi/Git/RaspberryPiBot/bots/traffic.py >> /home/pi/bot.log 2>&1 &
