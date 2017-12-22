6d52f072b54a8a0b817dd3c00644fb286fac9ceb
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-12-22 09:42:59 +0100

    add test traffic bot which sends warnings

---
+++ bots/traffic_test.py
@@ -0,0 +1,87 @@
+import requests
+from bs4 import BeautifulSoup
+import telepot
+from telepot.loop import MessageLoop
+import time
+import re
+
+
+token = '204922160:AAFZhl9leT5cMovHqalSbCvpAuD-uyuHtpw'
+chat_id = 322086570
+bot = telepot.Bot(token)
+url = 'http://gateway.engel/traffic.php'
+regex = '[0-9]{1,5}'
+
+
+def traffic():
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    tt = soup.find_all('tt')[0].text
+    numbers = re.findall(pattern=regex, string=tt)
+    bleiben_noch = int(numbers[5])
+    # summary = '%i MB' % (bleiben_noch)
+    # print(summary)
+    return bleiben_noch
+
+
+def warning():
+    return
+
+
+class stopwatch():
+    def __init__(self, day, hour, minute):
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+
+    def start(self):
+        return 'Starte Intervall.'
+
+    def stop(self):
+        day = time.localtime()[2]
+        hour = time.localtime()[3]
+        minute = time.localtime()[4]
+        return 'Das muss noch implementiert werden.'
+
+    def exists():
+        return True
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if msg['text'] == '/traffic':
+        bot.sendMessage(
+            chat_id=chat_id,
+            text='{} MB'.format(traffic()),
+        )
+    else:
+        bot.sendMessage(
+            chat_id=chat_id,
+            text='Usage:\n/traffic',
+        )
+    # elif msg['text'] == '/start':
+    #     watch = stopwatch(time.localtime()[2], time.localtime()[3], time.localtime()[4])
+    #     bot.sendMessage(chat_id=chat_id, text=watch.start())
+    # elif msg['text'] == '/stop':
+    #     try:
+    #         bot.sendMessage(chat_id=chat_id, text=watch.stop())
+    #     except:
+    #         bot.sendMessage(chat_id=chat_id, text='Zuerst das Intervall mit /start starten.')
+
+
+# Keep the bot listening and running
+print('listening ...')
+# bot.message_loop(handle)
+# while True:
+#     time.sleep(1000)
+MessageLoop(bot, handle).run_as_thread()
+while True:
+    aktuell = traffic()
+    if aktuell > 1500:
+        time.sleep(60 * 60)
+    else:
+        bot.sendMessage(
+            chat_id=322086570,
+            text='{} MB'.format(aktuell),
+        )
+        time.sleep(60 * 30)
